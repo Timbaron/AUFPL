4a4ed0551127bc46d69b87c9c8046da3697298c0
timbaron <timothy.akiode@student.aul.edu.ng>
2022-08-24 23:59:32 +0100

    Create Club and Player MSC

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(20)->create();
+        \App\Models\Club::factory(6)->create();
+        \App\Models\Player::factory(60)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
