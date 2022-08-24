7496dc04d566df68c066c4e2219d2f2f244d2830
timbaron <timothy.akiode@student.aul.edu.ng>
2022-08-24 23:38:07 +0100

    add info to home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,5 +3,27 @@
 @section('title', 'Anchor University Premier League')
 
 @section('content')
-    This is a content section
+<div class="jumbotron mt-3 jumbo">
+    <h1 class="display-8">Gameweek 1</h1>
+
+    <p class="lead">
+        <a class="btn btn-secondary btn-lg" href="#" role="button">My Team</a>
+    </p>
+    <div class="infos">
+        <div class="info">
+            Highest Points: 232
+        </div>
+        <div class="info">
+            Averaged Points: 32
+        </div>
+        <div class="info">
+            MOTW:
+            Akiode Timothy
+        </div>
+        <div class="info">
+            Most Captained:
+            Donkeeng
+        </div>
+    </div>
+</div>
 @endsection
